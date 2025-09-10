5330a32fda12763b9a074f6d6747c838609aa632
enzof <enzof@COMPUTADORA-ENZO>
2025-09-09 21:16:55 -0300

    commit 3

---
+++ Desempeños Java/src/Desempeño_1.java
@@ -0,0 +1,11 @@
+
+// 1. Generar un valor aleatorio entre 0 y 1000. Mostrar la cantidad de dígitos que tiene dicho número.
+
+public class Desempeño_1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
