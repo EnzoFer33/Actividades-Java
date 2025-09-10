6b97c248b3ee1fb8ada3d1e1a538c9bd098e5a47
enzof <enzof@COMPUTADORA-ENZO>
2025-09-09 21:07:44 -0300

    commit 2

---
+++ Desempeños Java/src/Problema_1.java
@@ -0,0 +1,13 @@
+
+public class Problema_1 {
+
+	public static void main(String[] args) {
+		int x=1;
+		while (x<=100) {
+			System.out.print(x+"-");
+			x=x+1;
+		}
+
+	}
+
+}
---
+++ Desempeños Java/src/Problema_3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problema_3 {
+    public static void main(String[] ar) {
+        Scanner teclado = new Scanner(System.in);
+        int x, suma, valor, promedio;
+        x = 1;
+        suma = 0;
+        while (x <= 5) {
+            System.out.print("Ingrese un valor:");
+            valor = teclado.nextInt();
+            suma = suma + valor;
+            x = x + 1;
+        }
+
+        promedio = suma / 5;
+
+        System.out.print("La suma de los 5 valores es:");
+        System.out.println(suma);
+        System.out.print("El promedio es:");
+        System.out.print(promedio);
+        
+        teclado.close();
+    }
+}
